d31bb5e3fcc24c509f9904e191067f0683e74b2f
Louis Kueh <jovanhan2@gmail.com>
2021-02-10 15:21:59 +0000

    feat(Select Component): working example

--- src/components/SelectComponent.tsx
+++ src/components/SelectComponent.tsx
@@ -1,12 +1,13 @@
 import Select from 'react-select'
 
-function convertToSelectObject(object: any) {
-  return { value: object.name, label: object.name }
+function convertToSelectObject(object:any){
+  return {value: object.name, label: object.name}
 }
 
-function SelectComponent(props: selectProps) {
-
-  const resultConvert = props.items.map(convertToSelectObject);
+function SelectComponent (props:selectProps){
+  console.log("Select component " + JSON.stringify(props.items))
+  const resultConvert = props.items.map( convertToSelectObject );
+  console.log("result convert is ")
   return (
     <Select options={resultConvert} />
   )
